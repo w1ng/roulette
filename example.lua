b32c3902c2bb6ff08852e0cfaad4627e43a66f55
w1ng <525163481@qq.com>
2017-06-22 17:51:37 +0800

    Update example.lua
    fix bug

--- example.lua
+++ example.lua
@@ -1,13 +1,13 @@
 local roulette = require("roulette")
-local tbl = {{"a",0.6},{"b",0.3},{"b",0.1}}
+local tbl = {{"a",0.6},{"b",0.3},{"c",0.1}}
 roulette.alias(tbl)
 
 local a,b,c = 0,0,0
 for i=1,10000 do
 	local temp = roulette.next()
-	if temp == 1 then
+	if temp == "a" then
 		a = a+1
-	elseif temp == 2 then
+	elseif temp == "b" then
 		b = b+1
 	else
 		c = c+1
